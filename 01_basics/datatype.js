197588994b0c7a9b617ca87135ac04314beaaa23
Raushan-kumar20 <159602571+Raushan-kumar20@users.noreply.github.com>
2024-06-14 05:30:44 +0000

    container congfig done

---
+++ 01_basics/datatype.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all js code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log( 3 + 
+    3) // code readability should be high
+
+console.log("Raushan")
+
+
+
+let name = "raushan"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bight
+//string =>""
+// boolean =>true/false
+//null => standlone value
+// undefined =>
+
+
+//object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
+
+
+
+
